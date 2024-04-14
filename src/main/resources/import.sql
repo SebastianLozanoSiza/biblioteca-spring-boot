829d19dfff4bf957b4b75308ac00335da85b6268
sebas-1207 <jebastianlozano1207@outlook.com>
2024-04-13 22:11:35 -0500

    Agregado el jwt y el swagger(falta documentar bien)

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -11,16 +11,16 @@ INSERT INTO libros (titulo, autor, genero, publicacion, cantidad_disponible) VAL
 INSERT INTO libros (titulo, autor, genero, publicacion, cantidad_disponible) VALUES ('Crimen y castigo', 'Fiodor Dostoievski', 'Novela', '1866-01-01', 2);
 
 -- Inserts para la entidad Usuario
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('1234567890', 'Juan', 'Perez', 'juan@example.com', 'contraseña', '123456789');
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('0987654321', 'Maria', 'Gomez', 'maria@example.com', 'contraseña', '987654321');
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('1122334455', 'Pedro', 'Rodriguez', 'pedro@example.com', 'contraseña', '1122334455');
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('5544332211', 'Ana', 'Martinez', 'ana@example.com', 'contraseña', '5544332211');
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('7788990011', 'Luis', 'Lopez', 'luis@example.com', 'contraseña', '7788990011');
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('6677889900', 'Laura', 'Gonzalez', 'laura@example.com', 'contraseña', '6677889900');
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('3355778899', 'Carlos', 'Sanchez', 'carlos@example.com', 'contraseña', '3355778899');
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('9900112233', 'Sofia', 'Fernandez', 'sofia@example.com', 'contraseña', '9900112233');
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('2244668800', 'Elena', 'Rojas', 'elena@example.com', 'contraseña', '2244668800');
-INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('8877665544', 'Pablo', 'Ramirez', 'pablo@example.com', 'contraseña', '8877665544');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('1234567890', 'Juan', 'Perez', 'juan@gmail.com', 'juan123', '123456789');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('0987654321', 'Maria', 'Gomez', 'maria@example.com', 'to_be_continued', '987654321');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('1122334455', 'Pedro', 'Rodriguez', 'pedro@gmail.com', 'pedro123', '1122334455');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('5544332211', 'Ana', 'Martinez', 'ana@example.com', 'to_be_continued', '5544332211');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('7788990011', 'Luis', 'Lopez', 'luis@gmail.com', 'luis123', '7788990011');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('6677889900', 'Laura', 'Gonzalez', 'laura@example.com', 'to_be_continued', '6677889900');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('3355778899', 'Carlos', 'Sanchez', 'carlos@example.com', 'to_be_continued', '3355778899');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('9900112233', 'Sofia', 'Fernandez', 'sofia@example.com', 'to_be_continued', '9900112233');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('2244668800', 'Elena', 'Rojas', 'elena@example.com', 'to_be_continued', '2244668800');
+INSERT INTO usuarios (cedula, nombre, apellido, email, pwd, telefono) VALUES ('8877665544', 'Pablo', 'Ramirez', 'pablo@example.com', 'to_be_continued', '8877665544');
 
 -- Inserts para la entidad Roles
 INSERT INTO roles (nombre_rol, id_usuario) VALUES ('ROLE_ADMIN', 1);      
